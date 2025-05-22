58032496e6315c2c25c8538c4bc286b9a5691c33
priyacse53162 <priyacse53162@gmail.com>
2025-05-22 12:13:32 +0530

    solution

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import {Formik, Form, Field ,ErrorMessage} from "formik"
+import * as Yup from "yup"
+function App() {
+  const validationform = Yup.object().shape({
+    username: Yup.string().required("enter username"),
+    email:Yup.string().email("enter valid email").required("enter email"),
+    password:Yup.string().min(8,"minimum 8 letter").required("enter password"),
+    cpassword: Yup.string().oneOf([Yup.ref("password"),null]).required("enter confirm password")
+  })
+  const initialvalues={
+    username:"",
+    email:"",
+    password:"",
+    cpassword:""
+  }
+  const handlesubmit=(values)=>{
+    console.log(values)
+  }
+  return (
+    <div className="bgcontainer">
+    <Formik 
+      initialValues={initialvalues}
+      validationSchema={validationform}
+      onSubmit={handlesubmit}>
+      {({errors, touched})=>(
+        <Form className="form-container">
+        <label className="input-label">Username:</label>
+        <Field className="input-field" name='username' />
+        <ErrorMessage className='errormsg' name='username' component="div"/>
+        <label className="input-label">Email:</label>
+        <Field className="input-field" name='email' />
+        <ErrorMessage className='errormsg' name='email' component="div"/>
+        <label className="input-label">Password:</label>
+        <Field className="input-field" name='password' />
+        <ErrorMessage className='errormsg' name='password' component="div"/>
+        <label className="input-label">Confirm Password:</label>
+        <Field className="input-field" name='cpassword' />
+        <ErrorMessage className='errormsg' name='cpassword' component="div"/>
+        <button className="submit-button" type="submit">Submit</button>
+      </Form>
+      )}
+    </Formik>
+    </div>
+  )
+}
+
+export default App
